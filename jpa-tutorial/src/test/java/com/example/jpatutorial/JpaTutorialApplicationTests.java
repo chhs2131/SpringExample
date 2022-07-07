217ef5803c51a9efd58e42a51c1b942f9d84d72a
Basak <chgy2131@naver.com>
2022-07-07 12:43:29 +0900

    jpa-tutorial

---
+++ jpa-tutorial/src/test/java/com/example/jpatutorial/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
