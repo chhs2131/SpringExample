8f63f3d205daea3cc8aa64681fe8c353a11b1b8e
Basak <chgy2131@naver.com>
2022-09-24 16:09:33 +0900

    인프런/2 - DB 접근을 위한 설정 파일 추가

---
+++ inflearn/2 member-management-example/src/main/java/com/example/membermanagementexample/MemberManagementExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.membermanagementexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberManagementExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberManagementExampleApplication.class, args);
+	}
+
+}
---
+++ inflearn/2 member-management-example/src/test/java/com/example/membermanagementexample/MemberManagementExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.membermanagementexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberManagementExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
