2ac0c3267bbaf2816cfc67fdf5e9c0d6ed3eb4e4
Basak <chgy2131@naver.com>
2022-09-28 23:36:47 +0900

    [docs]

---
+++ gps_data_tutorial/src/test/java/com/example/gps_data_tutorial/GpsDataTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gps_data_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsDataTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ img_to_base64_tutorial/src/test/java/com/example/img_to_base64_tutorial/ImgToBase64TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.img_to_base64_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgToBase64TutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
